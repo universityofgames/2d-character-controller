5512c88dac798df8370ca45d41017df7b883cb83
Leszek <leszek@strixspace.com>
2021-09-05 22:17:38 +0200

    :bug: Collision detection has been improved

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class CharacterController2D : MonoBehaviour {
+    [Range(0, .3f)] [SerializeField, Tooltip("How much to smooth out the movement")] 
+    private float m_MovementSmoothing = .05f; 
+    
     [SerializeField, Tooltip("Player's maximum speed in seconds")]
     float speed = 5;
     
@@ -17,6 +21,17 @@ public class CharacterController2D : MonoBehaviour {
     [SerializeField, Tooltip("Slowing the player down when he stops moving on the ground")]
     float groundDeceleration = 25;
     
+    [SerializeField] 
+    private LayerMask whatIsGround;	
+    
+    [SerializeField] 
+    private Transform groundCheck;
+    
+    [Header("Events")]
+    [Space]
+    public UnityEvent OnLandEvent;
+    
+    const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
     private BoxCollider2D boxCollider;
     private Vector2 velocity;
     private bool grounded;
@@ -24,7 +39,7 @@ public class CharacterController2D : MonoBehaviour {
     private void Awake() { 
         boxCollider = GetComponent<BoxCollider2D>();
     }
-
+    
     private void Update()  {
         // Manage player if it touches the ground
         IsGrounded();
@@ -39,49 +54,42 @@ public class CharacterController2D : MonoBehaviour {
             ? Mathf.MoveTowards(velocity.x, speed * moveInput, acceleration * Time.deltaTime)
             : Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
 
-        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        if (!grounded)
+            velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        
         transform.Translate(velocity * Time.deltaTime);
-
-        // Check if player has touched the ground
-        IsTouchGround();
     }
+    
+    private void FixedUpdate()
+    {
+        bool wasGrounded = grounded;
+        grounded = false;
 
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+    }
+    
     private void IsGrounded()
     {
         if (grounded) {
             velocity.y = 0;
 
             if (Input.GetButtonDown("Jump")) {
+                grounded = false;
                 // Calculate the velocity required to achieve the target jump height.
                 velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
             }
         }
     }
 
-    private void IsTouchGround()
-    {
-        // Retrieve all colliders to check that the player has touched the ground
-        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
-        grounded = false;
-        
-        foreach (Collider2D hit in hits)
-        {
-            // Ignore our own collider
-            if (hit == boxCollider)
-                continue;
-
-            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
-
-            // Check if we are still overlapping this collider.
-            // The overlap may no longer exist due to another intersected collider pushing us out of this one.
-            if (colliderDistance.isOverlapped)
-            {
-                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
-
-                // If we intersect an object beneath us, it means that we have touched the ground
-                if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && velocity.y < 0)
-                    grounded = true;
-            }
-        }
-    }
 }
